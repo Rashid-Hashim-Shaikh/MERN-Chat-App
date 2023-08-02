22c2c16a7db5546e6e32a9278f7a538b1a0a015d
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2023-08-03 01:12:40 +0530

    Created Register API

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv")
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
 
-
-// dotenv.config();
+// to access the .env files
+dotenv.config();
 connectDB();
+
 const app = express();
+
+// to accept json as request
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is Running succesfully")
 })
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
+app.use('/api/user', userRoutes)
 
-
-})
-app.get("/api/chat/:id", (req, res) => {
-
-    const singleChat = chats.find((chat) => chat._id === req.params.id)
-    res.send(singleChat)
-})
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server Started on Port ${PORT}`))
